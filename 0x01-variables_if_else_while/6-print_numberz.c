9f656cb0e3866277c0d4afd7b4095615396cca7e
Alvinimbua <bulimua@gmail.com>
2022-03-13 15:49:10 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar('\n');
+	putchar(a);
+
+	return (0);
+}
