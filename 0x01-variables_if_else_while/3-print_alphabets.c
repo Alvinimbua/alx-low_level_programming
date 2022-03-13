f6acae15c2f1ee9d5810e698bdce53123e057bd6
Alvinimbua <bulimua@gmail.com>
2022-03-13 11:59:00 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int ch;
 
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
 	return (0);
 }
