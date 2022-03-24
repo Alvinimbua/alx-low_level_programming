fa222a45335a98b451f5ddfd0dac40f6b5b6235b
AlvinImbua <bulimua@gmail.com>
2022-03-25 01:40:49 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
  * _strcat - concatenate two strings
- * @dest: firdt parameter
+ * @dest: first parameter
  * @src: second parameter
  * Return: returns a char
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	for(i = 0; dest[i] != '\0'; i++);
-	for(j = 0; src[j] != '\0'; j++)
-		dest[i+j] = src[j];
-	dest[i+j] = '\0';
 
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
 
 	return (dest);
 }
