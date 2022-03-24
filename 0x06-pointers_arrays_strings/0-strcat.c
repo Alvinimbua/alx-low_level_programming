56793096372835de09e672a3b605c1ad19cf8342
AlvinImbua <bulimua@gmail.com>
2022-03-25 00:41:29 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -25,14 +25,11 @@ int length(char *i)
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
+	for(i = 0; dest[i] != '\0'; i++);
+	for(j = 0; src[j] != '\0'; j++)
+		dest[i+j] = src[j];
+	dest[i+j] = '\0';
 
-	i = length(dest);
-	for  (j = 0; src[j] != '\0'; ++j)
-	{
-		dest[i] = src[j];
-		++1;
-	}
-	dest[i] = '\0';
 
 	return (dest);
 }
